819f9fe5ca26364da95f5effa95a9dbe406c85c0
millalin <milla.lintunen@hotmail.com>
2020-02-26 20:40:42 +0200

    iframe adding

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+const helmet = require('helmet')
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const router = require('./router')
+
+app.use(helmet.frameguard({ action: 'SAMEORIGIN' }))
 
 app.use(cors())
 app.use(bodyParser.json())
