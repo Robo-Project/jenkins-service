ce970e433450c561810207f70fb4336837063865
Pasi Alho <alho.pasi@iki.fi>
2020-04-06 16:09:24 +0300

    Revert "Add https"
    This reverts commit e5e31ab6191811b2a9fff9dd24c990a978af7e87.

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
-const https = require('https')
+const http = require('http')
 const app = require('./app')
-const fs = require('fs')
 
 const config = {
   PORT: 4000
 }
 
-const credentials = {
-  key: fs.readFileSync('certs/robo.key', 'utf8'),
-  cert: fs.readFileSync('certs/robo.crt', 'utf8')
-}
-
-const server = https.createServer(credentials, app)
+const server = http.createServer(app)
 
 server.listen(config.PORT, () => {
-  console.log(`Server running on port ${config.PORT}`)
+    console.log(`Server running on port ${config.PORT}`)
 })
